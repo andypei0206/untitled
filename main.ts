96d96e9a047c2fa5b6abb9edd2fe3a2ece16f432
andypei0206 <71689509+andypei0206@users.noreply.github.com>
2020-10-13 14:33:54 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,43 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showString("0")
+    basic.pause(100)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showString("0")
+    basic.pause(100)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showString("0")
+    basic.pause(100)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.FreeFall, function () {
+    basic.showString("0")
+    basic.pause(100)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("0")
+    basic.pause(100)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showString("0")
+    basic.pause(100)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showString("0")
+    basic.pause(100)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showString("0")
+    basic.pause(100)
+    basic.clearScreen()
+})
+basic.forever(function () {
+	
+})
